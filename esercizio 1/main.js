6696552f4daf39534a4ca404407b14fda5a38384
MDEFEDI <Matteo_Defedilta@vfc.com>
2023-10-05 12:49:34 +0200

    moved the script main.js in the head and modified input to span

--- esercizio 1/main.js
+++ esercizio 1/main.js
@@ -1,25 +1,27 @@
-    const decreaseButton = document.querySelector('.decrease');
-    const increaseButton = document.querySelector('.increase');
-    const countInput = document.querySelector('.count');
-    const resetButton = document.querySelector('.reset');
+(function () {
+  const decreaseButton = document.querySelector('.decrease');
+  const increaseButton = document.querySelector('.increase');
+  const countInput = document.querySelector('.count');
+  const resetButton = document.querySelector('.reset');
 
-    let count = 0;
+  let count = 0;
 
-    function updateCount() {
-        countInput.value = count;
-    }
+  function updateCount() {
+    countInput.textContent = count;
+  }
 
-    increaseButton.addEventListener('click', () => {
-        count++;
-        updateCount();
-    });
+  increaseButton.addEventListener('click', () => {
+    count++;
+    updateCount();
+  });
 
-    decreaseButton.addEventListener('click', () => {
-        count--;
-        updateCount();
-    });
+  decreaseButton.addEventListener('click', () => {
+    count--;
+    updateCount();
+  });
 
-    resetButton.addEventListener('click', () => {
-        count = 0;
-        updateCount();
-    });
+  resetButton.addEventListener('click', () => {
+    count = 0;
+    updateCount();
+  });
+})();
