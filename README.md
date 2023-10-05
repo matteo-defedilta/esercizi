2d1ad36557c7b3edd93919769d06ff9e2c146178
MDEFEDI <Matteo_Defedilta@vfc.com>
2023-10-05 10:35:08 +0200

    first exercise

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
-# esercizi
+# esercizio 1
+
+0) heading con il nome dell'app
+
+1) pulsante di decremento di 1 (lato sinistro -)
+
+2) stampa a video del valore (al centro numero)
+
+3) pulsante di incremento di 1 (lato destro +)
+
+3) pulsante di reset del valore del contatore (in basso)
+
+ 
+
+I componenti vanno scritti direttamente in un file html (NON vanno creati dinamicamente in javascript)
+
+Aggiungi un file CSS di base e gestisci la logica in un file JS separato.
+
+Crea un repository githup e inoltrami il link quando finisci
---
+++ esercizio 1/main.js
@@ -0,0 +1,25 @@
+    const decreaseButton = document.querySelector('.decrease');
+    const increaseButton = document.querySelector('.increase');
+    const countInput = document.querySelector('.count');
+    const resetButton = document.querySelector('.reset');
+
+    let count = 0;
+
+    function updateCount() {
+        countInput.value = count;
+    }
+
+    increaseButton.addEventListener('click', () => {
+        count++;
+        updateCount();
+    });
+
+    decreaseButton.addEventListener('click', () => {
+        count--;
+        updateCount();
+    });
+
+    resetButton.addEventListener('click', () => {
+        count = 0;
+        updateCount();
+    });
